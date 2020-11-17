2fa71106d94d31eeee3bb35fa48ace9c08139df9
nordiws <nordiws@gmail.com>
2020-11-17 07:57:43 -0300

    Updating api adress on client app

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'http://localhost:3001/',
+  baseURL: 'https://nordiws-finance-app.herokuapp.com/',
   headers: {
     'Content-type': 'application/json',
   },
